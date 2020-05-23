fb8d9ab16992900687831cab72b929db6eaa9ff1
zmjack <jack@zmland.com>
2020-05-23 18:49:10 +0800

    v0.3.0

---
+++ NAspNet/Identity/PasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace NAspNet.Identity
+{
+    public static class PasswordHasher
+    {
+        private static readonly PasswordHasher<object> Hasher = new PasswordHasher<object>();
+
+        public static string HashPassword(string password) => Hasher.HashPassword(null, password);
+        public static PasswordVerificationResult VerifyHashedPassword(string hashedPassword, string providedPassword) => Hasher.VerifyHashedPassword(null, hashedPassword, providedPassword);
+
+    }
+}
